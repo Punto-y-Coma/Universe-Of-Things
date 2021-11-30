27fe58200d05d51ad179de166367133b6c903301
V <V@MacBook-Pro-de-V.local>
2021-11-30 13:05:21 +0100

    added components to main app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,62 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+  <v-app id="inspire">
+    <v-app-bar app shrink-on-scroll>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-toolbar-title>Application</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">PRUEBA DE QUE ESTO FUNCIONA</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-main>
-      <router-view />
+      <v-container>
+        <v-row>
+          <v-col v-for="n in 24" :key="n" cols="3">
+            <v-card class="mx-auto" max-width="344" min-height="350px">
+              >
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                height="200px"
+              ></v-img>
+
+              <v-card-title> Top western road trips </v-card-title>
+
+              <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+
+              <v-card-actions>
+                <v-btn color="orange lighten-2" text> Explore </v-btn>
+
+                <v-spacer></v-spacer>
+
+                <v-btn icon @click="show = !show">
+                  <v-icon>{{
+                    show ? "mdi-chevron-up" : "mdi-chevron-down"
+                  }}</v-icon>
+                </v-btn>
+              </v-card-actions>
+
+              <v-expand-transition>
+                <div v-show="show">
+                  <v-divider></v-divider>
+
+                  <v-card-text>
+                    I'm a thing. But, like most politicians, he promised more
+                    than he could deliver. You won't have time for sleeping,
+                    soldier, not with all the bed making you'll be doing. Then
+                    we'll go with that data file! Hey, you add a one and two
+                    zeros to that or we walk! You're going to do his laundry?
+                    I've got to find a way to escape.
+                  </v-card-text>
+                </div>
+              </v-expand-transition>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
@@ -44,7 +66,7 @@ export default {
   name: "App",
 
   data: () => ({
-    //
+    show: false,
   }),
 };
 </script>

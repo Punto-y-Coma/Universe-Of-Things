aa4a73da920c5cb766de47a48ab27aba84c592da
Rubén Bertólez Chicote <ruben@MacBook-Pro-de-Ruben.local>
2021-11-30 12:11:30 +0100

    first upload to develop

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,7 @@
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">PRUEBA DE QUE ESTO FUNCIONA</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>

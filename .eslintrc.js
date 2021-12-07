68ddbd0836334ee325cf9c04dd766a3790596493
Raul <raulfernan000@gmail.com>
2021-12-07 15:39:20 +0100

    merged from develop

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,17 +10,10 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-<<<<<<< HEAD
-    'prettier/pretier': [
-      'eror',
-      {
-        'enOfLine': 'auto',
-=======
     'prettier/prettier': [
       'error',
       {
         'endOfLine': 'auto',
->>>>>>> develop
       }
     ]
   },
